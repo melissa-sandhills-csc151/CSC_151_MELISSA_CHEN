fbf7d313cf861cc52d99768e160647067dd821b4
Melissa Chen <melisc@unc.edu>
2025-06-29 22:25:34 +0800

    message

--- src/labs/example/physics/Main.java
+++ src/labs/example/physics/Main.java
@@ -1,12 +1,50 @@
 //@author: Melissa Chen
-//@date: 2025 June 20
-//@purpose: Lab 4; calling and printing each method
+//@date: 2025 June 28
+//@purpose: Lab 5; finding the hypotenuse and the angle of a triangle and determine if it's a 3-4-5; calculating the distance from Earth to Sun and see if it matches the known value
 
 package labs.example.physics;
 
 public class Main {
     public static void main(String[] args) {
         Physics physics = new Physics();
+
+        //defining x and y
+        int x = 3;
+        int y = 4;
+
+        //Part 1: calling the hypotenuse and angle arguments from Physics.java and using if-else statement to determine if angle information is valid
+        double hypotenuse = physics.getHypotenuseOfTriangle(x, y);
+        double angle = physics.getAngleInDegrees(x, y);
+
+        //printing the value of the hypotenuse and angle that are calculated in Physics.java class
+        System.out.println("Hypotenuse: " + hypotenuse);
+        System.out.println("Angle: " + angle + " degrees");
+
+        //if-else statement that is used to determine if the angle matches a 3-4-5 triangle
+        if (angle > 37 || angle < 36.87) {
+            physics.logInvalidAngleInfo(angle);
+        } else {
+            physics.logValidAngleInfo(angle);
+        }
+
+         // Part 2: Light speed calculation
+
+         //assigning velocity and time that's used for getDistance() calculation
+         double velocity = physics.getLightSpeedInMPH();
+         double time = physics.getTimeFromSunToEarthInHours();
+ 
+         //calculating thee calculatedDistance and restating the known distance
+         double calculatedDistance = physics.getDistance(velocity, time);
+         double knownDistance = physics.getKnownDistanceToEarth();
+ 
+         System.out.println("Calculated Earth-Sun Distance: " + calculatedDistance);
+ 
+         //checking if the calculatedDistance is the same as the knownDistance
+         if (Math.abs(calculatedDistance - knownDistance) > 0) {
+             physics.logEarthToSunInvalidDistance(calculatedDistance); //returns this if the distances are different
+         } else {
+             System.out.println("Calculated distance is valid and matches known Earth-Sun distance."); //if the distances are the same
+         }
 
         // Test each method and print results with units
         System.out.println("Distance: " + physics.getDistance(60, 2) + " miles");
--- src/labs/example/physics/Physics.java
+++ src/labs/example/physics/Physics.java
@@ -1,6 +1,6 @@
 //@author: Melissa Chen
 //@date: 2025 June 20
-//@purpose: Lab 4; stating physics methods and equations
+//@purpose: Lab 5; finding the hypotenuse and the angle of a triangle and determine if it's a 3-4-5; calculating the distance from Earth to Sun and see if it matches the known value
 
 package labs.example.physics;
 
@@ -9,72 +9,104 @@ import java.lang.Math;
 public class Physics {
     final static double GRAVITY = 9.81; // m/s^2
 
-    // Distance: x = vt
+    // Week 5 Lab starts here
+    
+    //defining hypotenuse
+    public double getHypotenuseOfTriangle(int x, int y) {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    //finding the angle between x and y
+    public double getAngleInDegrees(int x, int y) {
+        double radians = Math.atan2(y, x);
+        return radians * 180 / Math.PI;
+    }
+
+    //works with the if-else statement in Main.java. If the "if statement" isn't valid, the program will return the following message
+    public void logInvalidAngleInfo(double angle) {
+        System.out.println("logging the angle " + angle + " degrees. This is not a right angle.");
+    }
+
+    //If the "if statement" is valid, the program will return the following message
+    public void logValidAngleInfo(double angle) {
+        System.out.println("logging the angle " + angle + " degrees. This is a valid 3-4-5 triangle");
+    }
+
+    //defining distance
     public double getDistance(double velocity, double time) {
         return velocity * time;
     }
 
-    // Velocity: v = x/t
+     // Defining light speed in miles per hour
+     public double getLightSpeedInMPH() {
+        return 186282 * 3600; // convert miles/sec to miles/hour
+    }
+
+    // Time it takes for light to travel from Sun to Earth, in hours
+    public double getTimeFromSunToEarthInHours() {
+        double lightSpeedMPS = 186282;
+        double timeInSeconds = 92947266.72 / lightSpeedMPS; // distance / speed
+        return timeInSeconds / 3600; // convert to hours
+    }
+
+    // Defining known Earth-Sun distance
+    public double getKnownDistanceToEarth() {
+        return 92947266.72; // miles
+    }
+
+    public void logEarthToSunInvalidDistance(double distance) {
+        System.out.println("Calculated distance " + distance + " miles is invalid.");
+    }
+
+    // Other physics methods
     public double getVelocity(double distance, double time) {
         return distance / time;
     }
 
-    // Momentum: ρ = mv
     public double getMomentum(double mass, double velocity) {
         return mass * velocity;
     }
 
-    // Force: F = ma
     public double getForce(double mass, double acceleration) {
         return mass * acceleration;
     }
 
-    // Work: W = fd
     public double getWork(double force, double distance) {
         return force * distance;
     }
 
-    // Kinetic Energy: KE = 1/2 mv²
     public double getKineticEnergy(double mass, double velocity) {
         return 0.5 * mass * Math.pow(velocity, 2);
     }
 
-    // Potential Energy: PE = mgh
     public double getPotentialEnergy(double mass, double height) {
         return mass * GRAVITY * height;
     }
 
-    // Displacement: s = ut + 1/2 at²
     public double getDisplacement(double initialVelocity, double time, double acceleration) {
         return (initialVelocity * time) + (0.5 * acceleration * Math.pow(time, 2));
     }
 
-    // Velocity without time: v = √(u² + 2as)
     public double getVelocityWithoutTime(double initialVelocity, double acceleration, double displacement) {
         return Math.sqrt(Math.pow(initialVelocity, 2) + (2 * acceleration * displacement));
     }
 
-    // Displacement with average velocity: s = ((u + v)/2)t
     public double getDisplacementAvgVelocity(double initialVelocity, double finalVelocity, double time) {
         return ((initialVelocity + finalVelocity) / 2) * time;
     }
 
-    // Time from stopped position: t = √(2s/a)
     public double getTimeFromStoppedPosition(double displacement, double acceleration) {
         return Math.sqrt((2 * displacement) / acceleration);
     }
 
-    // Average velocity in uniform motion: v = s/t
     public double getAvgVelocityInUniformMotion(double displacement, double time) {
         return displacement / time;
     }
 
-    // Vertical velocity: v_y = v_0y - gt
     public double getVerticalVelocity(double initialVerticalVelocity, double time) {
         return initialVerticalVelocity - (GRAVITY * time);
     }
 
-    // Velocity vector of missile: v = √(v_x² + v_y²)
     public double getVelocityVectorOfMissile(double velocityX, double velocityY) {
         return Math.sqrt(Math.pow(velocityX, 2) + Math.pow(velocityY, 2));
     }
